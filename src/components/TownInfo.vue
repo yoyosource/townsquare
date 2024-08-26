b3ecdf4ef0a10bb89f7e690493c9c543eeced203
Marcus Jones <camuise@matchaflavored.site>
2024-08-26 12:42:09 -0400

    Update TownInfo to size using width in portrait
    Fixes the horizontal clipping that occurs to the demon head background image on mobile screens

--- src/components/TownInfo.vue
+++ src/components/TownInfo.vue
@@ -114,6 +114,11 @@ export default {
   background: url("../assets/demon-head.png") center center no-repeat;
   background-size: auto 100%;
 
+  @media (orientation: portrait) {
+    width: 30%;
+    background-size: 100% auto;
+  }
+
   li {
     font-weight: bold;
     width: 100%;
